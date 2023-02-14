634a5193153d7c3e504f05e7e622421d7912bde5
EnterOrion <orionwesson@gmail.com>
2023-02-14 22:59:48 +0100

    Change keyboardEvent code

--- src/modules/mobileFunctionality.js
+++ src/modules/mobileFunctionality.js
@@ -3,7 +3,7 @@ const search = document.getElementById("cityName");
 
 const addEvent = () => {
     search.addEventListener('keyup', (keyboardEvent) => {
-        if (keyboardEvent.code === 'Enter') {
+        if (keyboardEvent.code == 13) {
         element.blur();
     }
     });
