dbf7aa0991337a0fd5e18e5e179b4ec2358b78cf
EnterOrion <orionwesson@gmail.com>
2023-02-14 23:04:14 +0100

    Modify addEvent function

--- src/modules/mobileFunctionality.js
+++ src/modules/mobileFunctionality.js
@@ -3,8 +3,8 @@ const search = document.getElementById("cityName");
 
 const addEvent = () => {
     search.addEventListener('keyup', (keyboardEvent) => {
-        if (keyboardEvent.code == 13) {
-        element.blur();
+        if (keyboardEvent.code === 'Enter') {
+        search.blur();
     }
     });
 }
