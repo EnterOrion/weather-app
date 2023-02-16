152530bfac8a7517fb6a3742b5db8908b309d4c9
EnterOrion <orionwesson@gmail.com>
2023-02-17 00:27:13 +0100

    Change padding to rem

--- src/modules/mobileFunctionality.js
+++ src/modules/mobileFunctionality.js
@@ -1,9 +1,8 @@
-const form = document.querySelector("form");
 const search = document.getElementById("cityName");
 
 // Removes focus on input after user has entered the city
 const addEvent = () => {
-  form.addEventListener("keyup", (keyboardEvent) => {
+  search.addEventListener("keyup", (keyboardEvent) => {
     if (keyboardEvent.code === 13 || keyboardEvent.code === "Enter") {
       search.blur();
     }
