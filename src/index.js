a7b6f1efab2efcae0b02e9ef777e2f71ae9dc08a
EnterOrion <orionwesson@gmail.com>
2023-05-09 09:26:04 +0530

    Remove buggy function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./style/style.scss";
 import displayWeather from "./modules/weatherData";
-import addEvent from "./modules/mobileFunctionality";
 
 const main = function () {
-  addEvent();
   displayWeather();
 };
 
